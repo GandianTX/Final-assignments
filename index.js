7026ea41edd78f94bae67b313192a47a118069ec
heilong_lyhy <mjlyhy@outlook.com>
2023-05-28 01:10:19 +0800

    完工！

---
+++ index.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // const yorn = true
+  const observer = new IntersectionObserver(entries => {
+    const targetVisible = entries[0].isIntersecting;
+    const dhAll = document.getElementById('dh-all');
+
+    if (!targetVisible) {
+      dhAll.style.top = '0px';
+      window.addEventListener('wheel', event => {
+        const dhAll = document.getElementById('dh-all');
+        if (event.deltaY < 0) {
+          dhAll.style.top = '-74px';
+        } else {
+          dhAll.style.top = '0px';
+        }
+      });
+    } else {
+      dhAll.style.top = '-74px';
+      yorn=true
+    }
+  }, {
+    root: null,
+    threshold: 0
+  });
+
+  const target = document.getElementById('target');
+  observer.observe(target);
+
+});
